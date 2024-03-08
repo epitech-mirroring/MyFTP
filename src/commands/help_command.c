cec3873b97342f41a23775fcdec1f07463e19ecb
RenardFute <axel.eckenberg@yahoo.fr>
2024-03-08 10:24:51 +0100

    :sparkles: Finish help command by ignoring case for given argument

--- src/commands/help_command.c
+++ src/commands/help_command.c
@@ -8,6 +8,7 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <ctype.h>
 #include "commands/help_command.h"
 
 void register_help_command(void)
@@ -25,6 +26,13 @@ void register_help_command(void)
     ftp_command_registry(true, help_cmd);
 }
 
+static char *to_lower(char *str)
+{
+    for (size_t i = 0; i < strlen(str); i++)
+        str[i] = (char) tolower(str[i]);
+    return str;
+}
+
 void help_callback(ftp_server_t *server, ftp_client_t *client, char **args)
 {
     ftp_command_t ***commands = ftp_command_registry(false, NULL);
@@ -38,7 +46,7 @@ void help_callback(ftp_server_t *server, ftp_client_t *client, char **args)
         return;
     }
     for (int i = 0; (*commands)[i] != NULL; i++)
-        if (strcmp((*commands)[i]->name, args[0]) == 0) {
+        if (strcmp((*commands)[i]->name, to_lower(args[0])) == 0) {
             dprintf(client->socket, "214 %s\r\n",
                 (*commands)[i]->help);
             dprintf(client->socket, "214 Help OK.\r\n");
